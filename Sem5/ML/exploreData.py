116545866f78257965d6755e75fcfc417bfb8e09
unknown <20PW19@cs2k16.amcs.ws2k12.psgcs.edu>
2022-07-13 09:48:20 +0530

    ML problemst2

--- Sem5/ML/exploreData.py
+++ Sem5/ML/exploreData.py
@@ -1,6 +1,55 @@
 import pandas as pd
-datas = pd.read_csv("Z:/collegeThings/Sem5/ML/housing.xls")
-data = pd.DataFrame(datas, 14)
-data.head()
+import numpy as np
+from scipy.stats import pearsonr
+#from scipy.stats import kurtosis
 
-print(data)
+
+'''Iris Dataset Exploration'''
+#data = pd.read_csv("Z:/collegeThings/Sem5/ML/iris.csv")
+#data.head()
+#print(data.describe())
+#meanData = []
+#stdDevData = []
+#medianData = []
+#modeData = []
+#meanData = data.mean()
+#medianData = data.median()
+#modeData = data.mode()
+#stdDevData = np.std(data)
+#print(stdDevData)
+#df = pd.DataFrame(data)
+#print(df.skew())
+#print(df.kurt()) 
+#X = np.stack((data['sepal.length'], data['sepal.width']), axis=0)
+#print(np.cov(X))
+#X = np.stack((data['petal.length'], data['petal.width']), axis=0)
+#print(np.cov(X))
+#corr,_ = pearsonr(data['sepal.length'], data['sepal.width'])
+#print(corr)
+#corr,_ = pearsonr(data['petal.length'], data['petal.width'])
+#print(corr)
+
+'''Housing Price Prediction Dataset Exploration'''
+#data = pd.read_csv("Z:/collegeThings/Sem5/ML/data.csv")
+#data.head()
+#print(data.describe())
+#meanData = []
+#stdDevData = []
+#medianData = []
+#modeData = []
+#meanData = data.mean()
+#medianData = data.median()
+#modeData = data.mode()
+#stdDevData = np.std(data)
+#print(stdDevData)
+#df = pd.DataFrame(data)
+#print(df.skew())
+#print(df.kurt()) 
+#X = np.stack((data['price'], data['sqft_lot']), axis=0)
+#print(np.cov(X))
+#corr,_ = pearsonr(data['price'], data['sqft_lot'])
+#print(corr)
+
+
+
+
